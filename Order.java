27c03b007260cca962e54d5f5e8e44a2806fd8bc
radha <radhadevimunagapaka@gmail.com>
2025-06-23 11:53:43 +0530

    SAP-123 , Order and Customer files

---
+++ Order.java
@@ -0,0 +1,14 @@
+package com.spring;
+
+public class Order {
+
+	Product product;
+	
+	public Order(Product prd) {
+		this.product = prd;
+		}
+	public void display() {
+		System.out.println("Product Id = "+product.productId);
+		System.out.println("Product Name = "+product.productName);
+	}
+}
