0cb4f850cd3eefa26d6a26e1c4376479c38892fa
Venky <venkysingaraju5656@gmail.com>
2025-06-23 12:22:17 +0530

    changes

---
+++ Hello.java
@@ -0,0 +1,17 @@
+class Hello
+{
+public static void main(String [] args)
+{
+System.out.println("Hello Java");
+}
+}
+
+
+public class Siddu is pro
+{
+public static void main(String [] args)
+{
+System.out.println ("Siddu is Pro");
+{
+	{
+	}
