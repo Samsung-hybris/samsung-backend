e4e3f681b2ecabdc582f883ed0223cbf955ba6b4
Venky <venkysingaraju5656@gmail.com>
2025-06-23 22:26:59 +0530

    change2

---
+++ BagClass.java
@@ -0,0 +1,16 @@
+package com.java.airtel;
+
+public class BagClass extends Recharge {
+int bagId;
+String bagName;
+double bagPrice;
+int bagEntry;
+void bagdetails()
+{
+	System.out.println("bagid ="+ bagId);
+	System.out.println("bagname ="+ bagName);
+	System.out.println("bagprice ="+ bagPrice);
+	System.out.println("bagentry ="+ bagEntry);
+}
+}
+}}
